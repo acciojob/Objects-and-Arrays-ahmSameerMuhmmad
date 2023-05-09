0559a95d6b6ba2549d8f8efa64755b0de23612ac
Muhmmad Sameer <120492305+ahmSameerMuhmmad@users.noreply.github.com>
2023-05-09 21:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+let team=players;     // for refrance we use like this
+let team1=[...players];  // for generating copy of an element;
+let cap1=[...person];
+
 window.players = players;
 window.person = person;
 window.team = team;
