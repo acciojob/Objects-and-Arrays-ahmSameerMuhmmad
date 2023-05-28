9129d7af5241bf4224e87e99a35a8477b75ae7e5
Muhmmad Sameer <120492305+ahmSameerMuhmmad@users.noreply.github.com>
2023-05-28 13:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const players = ["John", "Bob", "Alice", "Poppy"];
 const person = {
   name: "John Doe",
   age: 80,
+  gender: 23
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
